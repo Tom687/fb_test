959bb2ee102da45b5a9f02f8e9c8f2e738b07816
Tom Pomarede <tompomarede@imac-de-tom-1.home>
2022-12-30 02:24:50 +0100

    First commit

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config({ path: './.env '});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+export default app;
