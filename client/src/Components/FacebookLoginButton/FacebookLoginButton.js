18324a37cbbc90090a0da974a2a72f4fb5b9a843
Tom Pomarede <tompomarede@imac-de-tom-1.home>
2022-12-31 09:23:08 +0100

    Fetching page accessToken via internal API POST request

--- client/src/Components/FacebookLoginButton/FacebookLoginButton.js
+++ client/src/Components/FacebookLoginButton/FacebookLoginButton.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function FacebookLoginButton({ /*isLoggedIn = false, setIsLoggedIn*/ }) {
+export default function FacebookLoginButton({ isLoggedIn = false, setIsLoggedIn }) {
   //const [isLoggedin, setIsLoggedin] = useState(false);
 
-  const onLoginClick = () => {
+  /*const onLoginClick = () => {
     console.info('LOGIN CLICKED !');
     //window.FB.login(...);
     window.FB.getLoginStatus(function(response) {
@@ -20,7 +21,7 @@ export default function FacebookLoginButton({ /*isLoggedIn = false, setIsLoggedI
       }
       console.log(4)
     });
-    /*window.FB.login(function(response) {
+    /!*window.FB.login(function(response) {
       console.log({response})
       if (response.authResponse) {
         setIsLoggedin(true);
@@ -30,26 +31,47 @@ export default function FacebookLoginButton({ /*isLoggedIn = false, setIsLoggedI
         setIsLoggedin(false);
         console.log('eeeeennnn');
       }
-    });*/
-  };
+    });*!/
+  };*/
 
-  console.log({isLoggedin})
+  console.log({isLoggedIn})
 
   window.checkLoginState = () => {
-    window.FB.getLoginStatus(function(response) {
+    window.FB.getLoginStatus(async function(response) {
       //statusChangeCallback(response);
       console.log(response);
-      if (response.authResponse) {
-        setIsLoggedin(true);
+      if (response.status === 'connected' || response.authResponse) {
+        const accessToken = response.authResponse.accessToken;
+        const userId = response.authResponse.userID;
+        setIsLoggedIn(true);
+
+        try {
+          const res = axios.post('http://localhost:1337/pages', {
+            client_id: '1242384183292030',
+            client_secret: 'e61a882707fe673a37d55a450486a68c',
+            fb_exchange_token: accessToken,
+            accessToken,
+            url: 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token',
+          }, {
+            headers: {
+              authorization: `Bearer ${accessToken}`
+            }
+          })
+
+          console.log({res})
+        }
+        catch (err) {
+          console.log('Err getPageToken :', err);
+        }
       }
       else {
-        setIsLoggedin(false);
+        setIsLoggedIn(false);
       }
     });
   };
 
   const onLogoutClick = async () => {
-    await window.FB.logout(() => setIsLoggedin(false));
+    await window.FB.logout(() => setIsLoggedIn(false));
     window.checkLoginState();
     window.location.reload();
   };
@@ -72,7 +94,7 @@ export default function FacebookLoginButton({ /*isLoggedIn = false, setIsLoggedI
        onlogin={() => checkLoginState()}
        ></div>*/}
       {
-        !isLoggedin &&
+        !isLoggedIn &&
         <div
           className="fb-login-button" data-width="300" data-size="large" data-button-type="continue_with"
           data-layout="default"
@@ -83,9 +105,9 @@ export default function FacebookLoginButton({ /*isLoggedIn = false, setIsLoggedI
         />
       }
       {
-        isLoggedin && <button onClick={onLogoutClick}>Logout</button>
+        isLoggedIn && <button onClick={onLogoutClick}>Logout</button>
       }
-      { isLoggedin ? <h4>Connecté</h4> : <h4>Déconnecté</h4> }
+      { isLoggedIn ? <h4>Connecté</h4> : <h4>Déconnecté</h4> }
 
     </div>
   );
