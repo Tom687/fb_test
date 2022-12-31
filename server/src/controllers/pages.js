371007f8d3116ac603aa4f7b5916be9e6a7b5125
Tom Pomarede <tompomarede@imac-de-tom-1.home>
2022-12-31 17:59:48 +0100

    Fixed URL to get users pages list + linked access tokens

--- server/src/controllers/pages.js
+++ server/src/controllers/pages.js
@@ -1,21 +1,35 @@
 import axios from 'axios';
 export const getPagesAccessToken = async (req, res, next) => {
-  const { client_id, client_secret, fb_exchange_token, accessToken, apiUrl } = req.body;
+  const { client_id, client_secret, fb_exchange_token, accessToken, apiUrl, userId } = req.body;
 
-  const url = `${apiUrl}&client_id=${client_id}&client_secret=${client_secret}&fb_exchange_token=${fb_exchange_token}`;
+  //const url =
+  // `${apiUrl}&client_id=${client_id}&client_secret=${client_secret}&fb_exchange_token=${fb_exchange_token}`;
 
-  const fetchAccessToken = await axios.get(url, {
+  console.log({...req.body})
+  const url = `https://graph.facebook.com/${userId}/accounts?access_token=${accessToken}`;
+
+  const fetchPagesAccessToken = await axios.get(url, {
     header: {
       authorization: `Bearer ${accessToken}`
     }
   });
 
-  const data = fetchAccessToken.data;
+  const data = fetchPagesAccessToken.data;
+
+  console.log({...data})
+
+  //const pagesData = data.map((page) => {
+  //  return {
+  //    pageAccessToken: page.access_token,
+  //  }
+  //})
+
 
   return res.status(200).json({
     userAccessToken: accessToken,
-    pageAccessToken: data.access_token,
-    pageAccessTokenType: data.token_type,
-    expiresIn: data.expires_in
+    //pageAccessToken: data.access_token,
+    //pageAccessTokenType: data.token_type,
+    //expiresIn: data.expires_in,
+    data
   });
 }
