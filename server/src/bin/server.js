ecf067f69f79c3beecde410fbe9c377aa1259490
Tom Pomarede <tompomarede@imac-de-tom-1.home>
2022-12-31 09:25:58 +0100

    Commented out unecessary stuff

--- server/src/bin/server.js
+++ server/src/bin/server.js
@@ -1,12 +1,12 @@
 import app from '../app.js';
 
 // Safety net 2 : If there are uncaught exceptions
-process.on('uncaughtException', err => {
-  console.log('Uncaught Exception. Shutting down…');
-  console.log(err.name, err.message);
-  // Shutting server down
-  process.exit(1); // 0 = success | 1 = Uncaught Exception
-});
+//process.on('uncaughtException', err => {
+//  console.log('Uncaught Exception. Shutting down…');
+//  console.log(err.name, err.message);
+//  // Shutting server down
+//  process.exit(1); // 0 = success | 1 = Uncaught Exception
+//});
 
 const port = process.env.PORT || 1337;
 
