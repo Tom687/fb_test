d2b891a87cc70a1601221644ea5107824299f938
Tom Pomarede <tompomarede@imac-de-tom-1.home>
2022-12-31 03:54:26 +0100

    Added FacebookLoginButton

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Posts from './Components/Posts/Posts';
+import FacebookLoginButton from './Components/FacebookLoginButton/FacebookLoginButton';
+
+//import { initializeFacebookSdk } from './utils/initFacebookSdk';
 
 function App() {
+  //initializeFacebookSdk();
+
   return (
     <div className="App">
       <h1>Test FB</h1>
+      <FacebookLoginButton />
       <Posts />
     </div>
   );
