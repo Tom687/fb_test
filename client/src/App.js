b18409a3cf91ee4be8f0904c935d67f2aaabc8a1
Tom Pomarede <tompomarede@imac-de-tom-2.home>
2023-01-02 14:55:28 +0100

    Cleaned code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Posts from './Components/Posts/Posts';
 import FacebookLoginButton from './Components/FacebookLoginButton/FacebookLoginButton';
