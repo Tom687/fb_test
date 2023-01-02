3ca40da856b914592280fce0783c75098689ddbf
Tom Pomarede <tompomarede@imac-de-tom-2.home>
2023-01-02 13:04:20 +0100

    Formated code

--- server/src/routes/pages.js
+++ server/src/routes/pages.js
@@ -3,7 +3,6 @@ import { getPagesAccessToken } from '../controllers/pages.js';
 
 const router = express.Router();
 
-//router.get('/')
 router.post('/', getPagesAccessToken);
 
 export default router;
