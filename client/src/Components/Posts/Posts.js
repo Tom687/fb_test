0dff314d1a24a19ca5dd2fadbb1404dfb8a3149a
Tom Pomarede <tompomarede@imac-de-tom-2.home>
2023-01-02 12:53:45 +0100

    Cleaned code

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
+// TODO : Passer pages en prop ? Pour ça que ça render en double ?
 export default function Posts({ isLoggedIn }) {
   const [posts, setPosts] = useState('');
 
@@ -10,23 +11,12 @@ export default function Posts({ isLoggedIn }) {
   const [selectedPage, setSelectedPage] = useState({});
 
   const getUserPages = async () => {
-
-  };
-  console.log({userPages})
-
-  console.log({selectedPage})
-
-  const getPosts = async (pageId) => {
     try {
-      //const res = await axios.get(`https://graph.facebook.com/ncla1ere?fields=id&access_token=${myUserToken}`);
-      const accessToken = selectedPage.access_token;
-      const pageAccessToken = localStorage.getItem('pageAccessToken');
-      console.log({accessToken})
+      const pageAccessToken = selectedPage.access_token;
+      const pageId = selectedPage.id;
 
-      // TODO : Pour récupérer l
-      // FIXME : ID de la page pas bon, comment récupérer la liste des pages de l'user ?
-      if (accessToken) {
-        const res = await axios.get(`https://graph.facebook.com/110201451952871/feed?access_token=${accessToken}`);
+      if (pageAccessToken) {
+        const res = await axios.get(`https://graph.facebook.com/v15.0/${pageId}/feed?access_token=${pageAccessToken}&since=0&limit=100`);
 
         return res.data;
       }
@@ -36,9 +26,11 @@ export default function Posts({ isLoggedIn }) {
     }
   };
 
+  // TODO : Modifier pour vider le state d'abord
   const handlePageSelect = (e) => {
     userPages.forEach((page, i) => {
       if (Object.values(userPages[i]).includes(e.target.value)) {
+        console.log({ page });
         setSelectedPage(page);
       }
     });
@@ -47,27 +39,27 @@ export default function Posts({ isLoggedIn }) {
   // TODO : getPost() après setSelectedPage
   useEffect(() => {
     if ((selectedPage && Object.keys(selectedPage).length > 0) && isLoggedIn) {
-      getPosts()
+      getUserPages()
         .then(res => {
-          console.log({...res.data})
-          if (res !== undefined) {
+          if (res && res !== undefined) {
+            // TODO : Fonction / requête pour récupérer les insights ici ?
             setPosts(res.data);
           }
         });
     }
   }, [selectedPage, isLoggedIn]);
 
-  console.log({posts})
-
   return (
     <div>
       Test posts :
       <form onSubmit={handlePageSelect}>
-        <select name="page" id="pageSelect" value={'selectedPage'} onChange={(e) => handlePageSelect(e)} value="options">
-          <option disabled={true} selected={true} value='options'>Sélectionnez la page</option>
+        <select
+          name="page" id="pageSelect" value={'selectedPage'} onChange={(e) => handlePageSelect(e)} value="options"
+        >
+          <option disabled={true} selected={true} value="options">Sélectionnez la page</option>
           {
             userPages && Object.keys(userPages).length > 0 && userPages.map((userPage, i) => (
-              <option value={userPage.id} key={userPage.id}>{ `${userPage.name} - ID : ${userPage.id}` }</option>
+              <option value={userPage.id} key={userPage.id}>{`${userPage.name} - ID : ${userPage.id}`}</option>
             ))
           }
         </select>
@@ -77,9 +69,9 @@ export default function Posts({ isLoggedIn }) {
           {
             selectedPage && Object.keys(selectedPage).length > 0 && (
               <>
-                <li key={selectedPage.id}>Nom de la page : { selectedPage.name }</li>
-                <li key={selectedPage.id}>ID de la page : { selectedPage.id }</li>
-                <li key={selectedPage.id}>Catégorie : { selectedPage.category_list[0].name }</li>
+                <li key={selectedPage.id}>Nom de la page : {selectedPage.name}</li>
+                <li key={selectedPage.id}>ID de la page : {selectedPage.id}</li>
+                <li key={selectedPage.id}>Catégorie : {selectedPage.category_list[0].name}</li>
               </>
             )
           }
