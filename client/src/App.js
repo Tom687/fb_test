98e85159523d1477631d8dd5f0da448844c462ff
Tom Pomarede <tompomarede@imac-de-tom-1.home>
2023-01-02 07:05:59 +0100

    Cleaned code

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import Posts from './Components/Posts/Posts';
-import FB from './Components/FacebookLoginButton/FB';
-import FB2 from './Components/FacebookLoginButton/FB2';
+//import FB from './Components/FacebookLoginButton/FB';
 import FacebookLoginButton from './Components/FacebookLoginButton/FacebookLoginButton';
-//import FacebookLoginButton2 from './Components/FacebookLoginButton/FacebookLoginButton2';
-//import FacebookLoginButton3 from './Components/FacebookLoginButton/FacebookLoginButton3';
 
 //import { initializeFacebookSdk } from './utils/initFacebookSdk';
 
 function App() {
   //initializeFacebookSdk();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  console.log({isLoggedIn})
+
   return (
     <div className="App">
       <h1>Test FB</h1>
@@ -20,10 +17,6 @@ function App() {
         isLoggedIn={isLoggedIn}
         setIsLoggedIn={setIsLoggedIn}
       />
-      {/*<FacebookLoginButton2 />
-      <FacebookLoginButton3 />*/}
-      <FB />
-      <FB2 />
       <Posts
         isLoggedIn={isLoggedIn}
       />
